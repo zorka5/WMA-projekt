bc9882a6d21e293ad3184d3d83d591cc9ffb6057
Zofia Kubrak <zofiakubrak@gmail.com>
2022-06-02 09:18:25 +0200

    utils added

--- frame_processing.py
+++ frame_processing.py
@@ -13,7 +13,6 @@ def background_substraction(frame: np.ndarray) -> np.ndarray:
     kernel2 = np.ones((4, 1), np.uint8)
     fgMask = cv2.erode(fgMask, kernel2, iterations=2)
     fgMask = cv2.dilate(fgMask, kernel2, iterations=3)
-
     return fgMask
 
 
@@ -35,15 +34,47 @@ def adaptive_threshold(frame: np.ndarray, background: np.ndarray) -> np.ndarray:
     return th2
 
 
+def mask_color(
+    frame: np.ndarray, lower_color_hsv: tuple[int, int, int], upper_color_hsv: tuple[int, int, int]
+) -> np.ndarray:
+    processed = frame
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+    mask = cv2.inRange(hsv_frame, lower_color_hsv, upper_color_hsv)
+    return mask
+
+
+def color_segmentation(frame) -> np.ndarray:
+    # maskowanie trwanika
+    trawnik1_mask = mask_color(frame, (60, 75, 120), (75, 175, 200))
+    trawnik2_mask = mask_color(frame, (70, 40, 120), (90, 120, 180))
+    trawnik_mask = cv2.bitwise_or(trawnik2_mask, trawnik1_mask)
+
+    # maskowanie drzew i krzewów
+    tuje1_mask = mask_color(frame, (40, 50, 50), (100, 250, 75))
+    tuje2_mask = mask_color(frame, (40, 25, 10), (80, 245, 150))
+    tuje_mask = cv2.bitwise_or(tuje1_mask, tuje2_mask)
+
+    # maskowanie chodnika
+
+    # maskowanie kory i bramy
+    kora_mask = mask_color(frame, (130, 40, 50), (175, 80, 230))
+    brama_mask = mask_color(frame, (0, 0, 50), (100, 75, 100))
+
+    brama_kora_mask = cv2.bitwise_or(brama_mask, kora_mask)
+    zielone_mask = cv2.bitwise_or(trawnik_mask, tuje_mask)
+
+    processed = cv2.bitwise_or(zielone_mask, brama_kora_mask)
+    processed = cv2.bitwise_not(processed)
+    fin = cv2.bitwise_and(frame, frame, mask=processed)
+    return fin
+
+
 def draw_box_contours(input_frame: np.ndarray, output_frame: np.ndarray) -> np.ndarray:
     contours, hierarchy = cv2.findContours(input_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print(len(contours))
-    blank = np.zeros(output_frame.shape[:3])
-
     output = cv2.drawContours(output_frame, contours, -1, (0, 0, 255), 1)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 100:
+        if cv2.contourArea(contour) < 1000:
             continue
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 3)
